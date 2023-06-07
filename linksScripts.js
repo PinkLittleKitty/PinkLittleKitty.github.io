79f3b7d581de4f570eb1c32826b2fadd6cdd2505
Santiago Fisela <71735491+PinkLittleKitty@users.noreply.github.com>
2023-06-07 14:08:41 -0300

    Create linksScripts.js

---
+++ linksScripts.js
@@ -0,0 +1,24 @@
+<!--Script por Ulises Alvarenga https://github.com/Ulysses-Alv -->
+
+function copyToClipboard(text, elementId, classes, textNode) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  const icono = document.getElementById(elementId);
+  icono.textContent = "Copied";
+
+  setTimeout(() => recreate(elementId, classes, textNode), 400)
+}
+
+function recreate(_elementId, _classes, _textNode) {
+  const miEnlace = document.getElementById(_elementId);
+  miEnlace.textContent = "";
+  const iconoDiscord = document.createElement("i");
+  iconoDiscord.className = _classes;
+  const textoEnlace = document.createTextNode(_textNode);
+  miEnlace.appendChild(iconoDiscord);
+  miEnlace.appendChild(textoEnlace);
+}
