0a802f64b9205d78ecfb84b45325c8f0995081e8
Santiago Fisela <santyfisela@gmail.com>
2025-03-11 00:31:13 -0300

    Cursor removal

--- linksScripts.js
+++ linksScripts.js
@@ -22,17 +22,3 @@ function recreate(_elementId, _classes, _textNode) {
   miEnlace.appendChild(iconoDiscord);
   miEnlace.appendChild(textoEnlace);
 }
-
-const cursor = document.querySelector('.cursor');
-
-        document.addEventListener('mousemove', e => {
-            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-        })
-
-        document.addEventListener('click', () => {
-            cursor.classList.add("expand");
-
-            setTimeout(() => {
-                cursor.classList.remove("expand");
-            }, 500)
-        })
